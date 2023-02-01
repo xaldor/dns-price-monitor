ffc5017d3876178b8c3db43daffb39cc8c338517
Artemy Efimov <artemyfmv@gmail.com>
2023-02-01 23:07:34 +0500

    write function to generate async database sessions

---
+++ src/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from fastapi.encoders import jsonable_encoder
+
+from src.settings import POSTGRES_URL
+
+
+engine: AsyncEngine = create_async_engine(
+    url=str(POSTGRES_URL), json_serializer=jsonable_encoder
+)
+
+
+async def get_database_session():
+    """Generates async database session. Intended to be used as a dependency."""
+    # NOTE: Such form of combining two context managers ensures
+    # that `session.rollback()` happens in case of any exception been raised,
+    # and `session.close()` at the end of working with session.
+    async with AsyncSession(engine) as session, session.begin():
+        yield session
--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,15 @@
 from starlette.config import Config
+from pydantic import PostgresDsn
 
 
 env = Config()
 
 # NOTE: APP_TITLE is used in OpenAPI documentation, but it's also just nice to have a project named :)
 APP_TITLE: str = env("APP_TITLE")
+
+# NOTE: POSTGRES_URL is constructed from its parts (scheme, user, etc.) in runtime.
+# It seems a little bit complicated, but still useful in case we need to get database URL components separatly.
+POSTGRES_URL: PostgresDsn = PostgresDsn(
+    f"{env('POSTGRES_SCHEME')}://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}@{env('POSTGRES_HOST')}:{env('POSTGRES_PORT')}/{env('POSTGRES_DB')}",
+    scheme=env("POSTGRES_SCHEME"),
+)
