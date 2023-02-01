d8c63c39b82cfe9daa0115fe675ca5b75d930f76
Artemy Efimov <artemyfmv@gmail.com>
2023-02-01 23:27:07 +0500

    define product schemas

---
+++ src/schemas/product.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, AnyHttpUrl, confloat
+from typing import TypeAlias, Optional
+
+
+ProductId: TypeAlias = int
+ProductTitle: TypeAlias = str
+ProductUrl: TypeAlias = AnyHttpUrl
+ProductDescription: TypeAlias = Optional[str]
+ProductRating: TypeAlias = Optional[confloat(ge=0.0, le=1.0)]
+
+
+class ProductInfo(BaseModel):
+    title: ProductTitle
+    url: ProductUrl
+    description: ProductDescription
+    rating: ProductRating
+
+
+class Product(ProductInfo):
+    id: ProductId
