32fb0100222569d611de1a2dd2b78cf18b446657
Artemy Efimov <artemyfmv@gmail.com>
2023-02-01 23:30:32 +0500

    define price record schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,2 @@
+from .product import *
+from .price_record import *
---
+++ src/schemas/price_record.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, conint
+from datetime import datetime
+from typing import TypeAlias
+
+
+PriceRecordId: TypeAlias = int
+Price: TypeAlias = conint(ge=0)
+PriceRecordTimestamp: TypeAlias = datetime
+
+
+class PriceRecordInfo(BaseModel):
+    price: Price
+    timestamp: PriceRecordTimestamp
+
+
+class PriceRecord(PriceRecordInfo):
+    id: PriceRecordId
