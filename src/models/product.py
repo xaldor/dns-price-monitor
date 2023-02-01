b4e74d83b7c12637a39a1b9f363f462b128ec707
Artemy Efimov <artemyfmv@gmail.com>
2023-02-02 00:34:01 +0500

    write methods to get/create products in database

--- src/models/product.py
+++ src/models/product.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
 from .base import BaseSQLModel
 from sqlalchemy import Column, Integer, Text, String, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Optional
+from pydantic import AnyHttpUrl
+
+
+from src.schemas import ProductInfo
+from src.exceptions import ProductAlreadyInMonitorList
 
 PRODUCT_TITLE_MAX_LEN: int = 256
 
@@ -19,3 +28,28 @@ class ProductSQLModel(BaseSQLModel):
         primaryjoin="PriceRecordSQLModel.product_id == ProductSQLModel.id",
         order_by="PriceRecordSQLModel.timestamp",
     )
+
+    # TODO: Combine `get_by_id` and `get_by_url` into one method
+    @classmethod
+    async def get_by_id(
+        cls, db: AsyncSession, target_id: int
+    ) -> Optional[ProductSQLModel]:
+        return await db.execute(select(cls).where(cls.id == target_id)).first()
+
+    @classmethod
+    async def get_by_url(
+        cls, db: AsyncSession, target_url: AnyHttpUrl
+    ) -> Optional[ProductSQLModel]:
+        return await db.execute(select(cls).where(cls.url == target_url)).first()
+
+    @classmethod
+    async def create(
+        cls, db: AsyncSession, product_info: ProductInfo
+    ) -> ProductSQLModel:
+        exists = await cls.get_by_url(db, product_info.url)
+        if exists:
+            raise ProductAlreadyInMonitorList
+        product = ProductSQLModel(**product_info.dict())
+        db.add(product)
+        db.flush()
+        return product
