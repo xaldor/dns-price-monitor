6e9239cf545365ef4adbc94722dedfa7ff5db354
Artemy Efimov <artemyfmv@gmail.com>
2023-02-02 18:06:47 +0500

    write basic view for products page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from src.settings import APP_TITLE, PRICE_UPDATE_INTERVAL_IN_MINUTES
 
 from src.utils import repeat_every
 from src.api.router import router
 from src.update_price import update_price
 
+from src.views import views
+
 app = FastAPI(title=APP_TITLE)
 
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
 app.include_router(router)
+app.include_router(views)
 
 
 @app.on_event("startup")
---
+++ src/views.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+from src.api.services.product import GetAllProducts
+from src.api.schemas.product import GetAllProductsRequest, GetAllProductsResponse
+
+views = APIRouter()
+
+templates = Jinja2Templates(directory="src/templates")
+
+
+@views.get("/")
+async def get_all_products_page(
+    request: Request,
+    service: GetAllProducts = Depends(GetAllProducts),
+    response_class=HTMLResponse,
+):
+    result = await service.process(GetAllProductsRequest())
+    return templates.TemplateResponse(
+        "products.html", {**result.dict(), "request": request}
+    )
